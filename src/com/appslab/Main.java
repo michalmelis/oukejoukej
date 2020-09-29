5ac98e7eb25cdded62c6ee444a73d96940080504
samuelzatovic <szatovic@gmail.com>
2020-09-29 09:10:25 +0200

    Finished the exercise.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,12 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        double heightOfstep = 0.2 ;
+        double lengthOfstep = 0.4;
+        double towerHeight = 100.00;
+        double totalDistance = (towerHeight/heightOfstep) * (lengthOfstep + heightOfstep);
+        System.out.println(totalDistance);
     }
 }
