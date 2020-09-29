5ea55b0b4738227fb36dd6d78058e8d324073fb4
samuelzatovic <szatovic@gmail.com>
2020-09-29 09:52:11 +0200

    Finished the exercise number fakin 222

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,27 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+
+        System.out.println(equal(1,1,1));
+
+    }
+     public static int equal( int a, int b, int c)
+    {
+
+
+        if  (a == b && b==c)
+        {
+            return  3;
+        }
+        else if ( a == b || b==c || a==c)
+        {
+        return  2;
+        }
+        else
+            {
+              return 0;
+            }
     }
 }
