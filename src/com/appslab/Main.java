8e6595ae92e7ca8432bbafb89fd96a66fd097ec3
samuelzatovic <szatovic@gmail.com>
2020-09-29 10:07:38 +0200

    Finished the exercise number fakin threee.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,14 @@
 package com.appslab;
 
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(isTriangle(1,2,3));
     }
+    public static boolean isTriangle(int a, int b, int c)
+    {
+        return (a+b)>c;
+    }
+
 }
