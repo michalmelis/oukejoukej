a976158cea1ffc02942eb3c3884bfefb38567e42
samuelzatovic <szatovic@gmail.com>
2020-10-06 09:52:20 +0200

    Finished the exercisee.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,19 @@
 package com.appslab;
 
+
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        System.out.println(Arrays.toString(multiplyByLength(new int[]{21, 54, 18, 523})));
+    }
+    public static int[] multiplyByLength(int[] array)
+    {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] * array.length;
+        }
+        return array;
     }
 }
