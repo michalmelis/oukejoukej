6c97c5f84aa4fbba12bd3f4ae58791f420bc2b49
michalmelis <michalmelis.sk@gmail.com>
2020-10-17 20:15:43 +0200

    Done.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,20 @@
 package com.appslab;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(rps("paper", "paper"));
+        System.out.println(rps("paper", "rock"));
+
+    }
+    public static String rps(String player1, String player2)
+    {
+        if (player1 == "rock" && player2 == "scissors" || player1 == "paper" && player2 == "rock" || player1 == "scissors" && player2 == "paper")
+            return "Player 1 wins";
+        if (player2 == "rock" && player1 == "scissors" || player2 == "paper" && player1 == "rock" || player2 == "scissors" && player1 == "paper")
+            return "Player 2 wins";
+        return "TIE";
     }
 }
