21e2558519234814b52483f223f5253f8d1aa263
michalmelis <michalmelis.sk@gmail.com>
2020-10-17 17:56:32 +0200

    Done.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,14 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+    public static void main(String[] args)
+    {
+        System.out.println(equalSlices(25,4,1));
     }
+    public static boolean equalSlices(int totalSlices, int  recipient, int slicesEach)
+    {
+        return totalSlices >= slicesEach*recipient;
+    }
+
 }
