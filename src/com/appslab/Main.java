6a03f82e954d669f22c6ee97345a419fc6284b0b
michalmelis <michalmelis.sk@gmail.com>
2020-10-17 21:17:33 +0200

    Done.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -8,16 +8,18 @@ public class Main
     public static void main(String[] args)
     {
        int largeArray[] = {5,4,6,8,84,8474,585,65,84,9,15,35,44,27,82,94};
-       int smallest = largeArray[0];
-       int largest  =  largeArray[0];
+       int answer = findMaxMin(largeArray);
+       System.out.println(answer);
+    }
 
-       for(int i = 0; i < largeArray.length; i++)
-       {
-           if (largeArray[i] < smallest) smallest = largeArray[i];
-           if (largeArray[i] > largest) largest = largeArray[i];
-       }
-
-       System.out.println(smallest+largest);
-
+    public static int findMaxMin(int[] array) {
+        int smallest = array[0];
+        int largest = array[0];
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] < smallest) smallest = array[i];
+            if (array[i] > largest)   largest = array[i];
+        }
+        return smallest + largest;
     }
 }
