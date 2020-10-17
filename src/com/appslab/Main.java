cc31de46c2dd663a31396ce8f38a9d84e554fbc0
michalmelis <michalmelis.sk@gmail.com>
2020-10-17 22:23:34 +0200

    Done.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,23 +1,31 @@
 package com.appslab;
 
-import java.util.Scanner;
-
 public class Main
 {
 
     public static void main(String[] args)
     {
-       int largeArray[] = {5,4,6,8,84,8474,585,65,84,9,15,35,44,27,82,94};
-       int smallest = largeArray[0];
-       int largest  =  largeArray[0];
-
-       for(int i = 0; i < largeArray.length; i++)
-       {
-           if (largeArray[i] < smallest) smallest = largeArray[i];
-           if (largeArray[i] > largest) largest = largeArray[i];
-       }
-
-       System.out.println(smallest+largest);
-
+        int cisielka[] = {5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243};
+        int iGotIt = warOfNumbers(cisielka);
+        System.out.println(iGotIt);
+    }
+    public static int warOfNumbers(int[] numbers)
+    {
+        int sumEven = 0, sumOdd = 0;
+        for (int i = 0; i < numbers.length; i++)
+        {
+            if (isItOdd(numbers[i])) {
+                sumOdd += numbers[i];
+            }
+            else {
+                sumEven +=  numbers[i];
+            }
+        }
+        return Math.abs(sumEven - sumOdd);
+    }
+    public static boolean isItOdd(int number){
+        if (number % 2 == 0)
+            return false;
+        return true;
     }
 }
