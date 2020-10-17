e26ca567b0f8cf23143afd84796ce34a5a0d5bd4
michalmelis <michalmelis.sk@gmail.com>
2020-10-17 19:03:53 +0200

    trying.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,27 @@
 package com.appslab;
+import java.lang.String;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.System.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        String a = "bummub";
+        System.out.println(Arrays.toString(checkPalindrome(new char[]('')));
+    }
+    public static boolean checkPalindrome(String[] a)
+    {
+        for (int i = 0; i < a.length/2; i++)
+        {
+            if (a[i] == a[a.length-1-i]) {
+                return false;
+            }
+
+
+        }
+        return true;
+
     }
 }
