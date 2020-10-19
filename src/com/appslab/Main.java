53207ece8f6e5e7dc494173a0ac8d9e2baa16714
michalmelis <michalmelis.sk@gmail.com>
2020-10-19 19:01:20 +0200

    Done

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,32 +1,17 @@
 package com.appslab;
 
-public class Main
-{
+public class Main {
 
     public static void main(String[] args)
     {
-        int cisielka[] = {5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243};
-        int iGotIt = warOfNumbers(cisielka);
-        System.out.println(iGotIt);
-    }
-    public static int warOfNumbers(int[] numbers)
-    {
-        int sumEven = 0, sumOdd = 0;
-        for (int i = 0; i < numbers.length; i++)
-        {
-            if (isItOdd(numbers[i])) {
-                sumOdd += numbers[i];
-            }
-            else {
-                sumEven +=  numbers[i];
-            }
-        }
-        return Math.abs(sumEven - sumOdd);
-    }
-    public static boolean isItOdd(int number){
-        if (number % 2 == 0)
-            return false;
+        String str1 = "bumbum";
+        String str2 = "bum";
+        System.out.println(endsWith(str1,str2));
 
-        return true;
+    }
+    public static boolean endsWith(String str1, String str2)
+    {
+     return (str1.endsWith(str2));
     }
 }
+
